9ec5f87e2b3acabb365b6018b59bf3a104d64ec5
Benjamin Lefaudeux <benjamin.lefaudeux@pm.me>
2022-04-27 10:58:50 -0700

    WIP

--- xformers/triton/k_fused_matmul_bw.py
+++ xformers/triton/k_fused_matmul_bw.py
@@ -29,7 +29,7 @@ from xformers.triton.sum_strided import sum_2d_dim_0
     key=["N"],
 )
 @triton.jit
-def kernel_bw(
+def kernel_bw_act(
     # Pointers to matrices
     GRAD_ACT, GRAD_OUT, ACT_INPUTS,
     # Matrix dimensions
@@ -82,6 +82,98 @@ def kernel_bw(
     tl.store(grad_act_ptrs, grad_act, mask=rn < N)
 
 
+# fmt: off
+@triton.autotune(
+    configs=[
+        triton.Config({"BLOCK_M": 16, "BLOCK_N": 16}, num_stages=5, num_warps=1),
+        triton.Config({"BLOCK_M": 32, "BLOCK_N": 32}, num_stages=5, num_warps=1),
+        triton.Config({"BLOCK_M": 64, "BLOCK_N": 32}, num_stages=5, num_warps=2),
+        triton.Config({"BLOCK_M": 32, "BLOCK_N": 64}, num_stages=5, num_warps=2),
+        triton.Config({"BLOCK_M": 128, "BLOCK_N": 64}, num_stages=4, num_warps=4),
+        triton.Config({"BLOCK_M": 64, "BLOCK_N": 128}, num_stages=4, num_warps=4),
+        triton.Config({"BLOCK_M": 128, "BLOCK_N": 128}, num_stages=3, num_warps=4),
+        # requires a GPU with enough shared memory
+        # triton.Config({"BLOCK_M": 32, "BLOCK_N": 256}, num_stages=3, num_warps=4),
+        # triton.Config({"BLOCK_M": 256, "BLOCK_N": 32}, num_stages=3, num_warps=4),
+        # triton.Config({"BLOCK_M": 64, "BLOCK_N": 256}, num_stages=3, num_warps=8),
+        # triton.Config({"BLOCK_M": 256, "BLOCK_N": 64}, num_stages=3, num_warps=8),
+    ],
+    key=["M", "N", "K"],
+)
+@triton.jit
+def kernel_matmul_transpose(
+    OUT, A, B,
+    M, N, K,
+    stride_on, stride_am, stride_bm,
+    # Meta-parameters
+    BLOCK_M: tl.constexpr,
+    BLOCK_N: tl.constexpr, GROUP_N: tl.constexpr,
+    BLOCK_K: tl.constexpr,
+):
+    # fmt: on
+
+    """
+    Kernel for computing Out = A^T x B
+
+    - A has shape (M, N)
+    - B has shape (M, K)
+    - Out has shape (N, K)
+
+    This kernel will consolidate over M
+    """
+
+    # programs are grouped together to improve L2 hit rate
+    # the logic is that we'll consolidate over K. If the programs were not grouped,
+    # then multiple cols/rows in the result would end up pulling in the same row and lines
+    # from the inputs. By grouping the computation we ensure some data reuse, which the hardware
+    # covers via the L2 cache
+    pid = tl.program_id(axis=0)
+
+    num_pid_n = tl.cdiv(N, BLOCK_N)  # number of program ids along the M axis
+    num_pid_k = tl.cdiv(K, BLOCK_K)  # number of programs ids along the N axis
+    num_pid_in_group = GROUP_N * num_pid_k  # number of programs in group
+    group_id = pid // num_pid_in_group  # id of the group this program is in
+    first_pid_n = group_id * GROUP_N  # row-id of the first program in the group
+    GROUP_N = min(
+        num_pid_n - first_pid_n, GROUP_N
+    )  # if `num_pid_m` isn't divisible by `GROUP_N`, the last group is smaller
+
+    # *within groups*, programs are ordered in a column-major order
+    # row-id /col-id of the program in the *launch grid*
+    pid_n = first_pid_n + (pid % GROUP_N)
+    pid_k = (pid % num_pid_in_group) // GROUP_N
+
+    # now compute the block that each program will go through
+    # rm (resp. rn) denotes a range of indices
+    # for rows (resp. col) of C
+    rn = pid_n * BLOCK_N + tl.arange(0, BLOCK_N)
+    rk = pid_k * BLOCK_K + tl.arange(0, BLOCK_K)
+    rm = tl.arange(0, BLOCK_M)
+
+    # the memory addresses of elements can follow numpy broadcasting
+    a_ptrs = A + rn[:, None]    # we transpose on the fly
+    b_ptrs = B + rk[None, :]
+
+    # initialize and iteratively update accumulator
+    acc = tl.zeros((BLOCK_N, BLOCK_K), dtype=tl.float32)
+
+    # block level matrix multiplication.
+    # We fetch a block memory block from both inputs, matmul and accumulate, then repeat
+    mask_rn = rn < N
+    mask_rk = rk < K
+
+    for i in range(0, M, BLOCK_M):
+        rm = tl.arange(0, BLOCK_M) + i
+        a = tl.load(a_ptrs + rm[None, :] * stride_am, mask=((rm[None, :] < M) & mask_rn[:, None]), other=0.0)
+        b = tl.load(b_ptrs + rm[:, None] * stride_bm, mask=((mask_rk[None, :] < K) & rm[:, None] < M), other=0.0)
+
+        acc += tl.dot(a, b)
+
+    # write back result
+    out_ptrs = OUT + rn[:, None] * stride_on + rk[None, :]
+    tl.store(out_ptrs, acc, mask=mask_rn[:, None] & mask_rk[None, :])
+
+
 def fused_matmul_backward(
     grad_out: torch.Tensor,
     inputs: torch.Tensor,
@@ -103,12 +195,11 @@ def fused_matmul_backward(
         grad_out = grad_out.contiguous()
 
     grad_out_ = grad_out if grad_out.ndim == 2 else grad_out.flatten(0, 1)
-    inputs_ = inputs if inputs.ndim == 2 else inputs.flatten(0, 1)
 
     assert grad_out_.shape[1] == weight.shape[0], "Incompatible dimensions in between grad_out and weight"
 
     M, N = grad_out_.shape
-    N, _ = weight.shape
+    N, K = weight.shape
 
     # Compute the gradient for the activation
     if activation_grad is not None:
@@ -122,7 +213,7 @@ def fused_matmul_backward(
         grid = lambda META: (M, triton.cdiv(N, META["BLOCK_N"])) # noqa
 
         # fmt: off
-        kernel_bw[grid](
+        kernel_bw_act[grid](
             grad_act, grad_out_, act_in,            # data ptrs
             N,                                      # shapes
             grad_act.stride(0), act_in.stride(0),   # strides
@@ -134,9 +225,26 @@ def fused_matmul_backward(
         # just before the activation
         grad_out_ = grad_act
 
+    # Compute the gradient for the weight
+    if trainable_weight:
+        grid_ = lambda META: (triton.cdiv(N, META["BLOCK_N"]) * triton.cdiv(K, META["BLOCK_K"]),) # noqa
+
+        grad_weight = torch.empty_like(weight)
+        inputs_ = inputs if inputs.ndim == 2 else inputs.flatten(0, 1)
+
+        # fmt: off
+        kernel_bw_act[grid_](
+            grad_weight, grad_out_, inputs_,        # data ptrs
+            M, N, K,                                # shapes
+            grad_weight.stride(0),
+            grad_out_.stride(0),
+            inputs_.stride(0)
+        )
+        # fmt: on
+
     # The following ops can also be handled by triton
     grad_in = grad_out_ @ weight
-    grad_weight = grad_out_.transpose(1, 0) @ inputs_ if trainable_weight else None
+
     grad_bias = sum_2d_dim_0(grad_out_) if trainable_bias else None
 
-    return grad_in.reshape_as(inputs), grad_weight, grad_bias
+    return grad_in.reshape_as(inputs), grad_weight if trainable_weight else None, grad_bias
